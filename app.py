97f56d2ae5f19d73dada1d2e7b105e9fb7e62d07
Sandun Chandrasiri <131155243+sandun-chandrasiri@users.noreply.github.com>
2023-06-02 19:37:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,14 @@ app = Flask(__name__)
 @app.route("/") 
 def index():
     return render_template("index.html")
+
+@app.route("/create-bucket", methods=['POST'])
+def create_bucket():
+    bucket_name = request.form["bucket_name"]
+    s3 = boto3.resource('s3')
+    bucket = s3.create_bucket(Bucket=bucket_name)
     
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True,port=8080,host='0.0.0.0')
