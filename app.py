3c886a17fb9ce5a9236cb1706c6cd4dd83f47863
Sandun Chandrasiri <131155243+sandun-chandrasiri@users.noreply.github.com>
2023-06-08 17:03:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,12 @@ def index():
 @app.route("/create-bucket", methods=['POST'])
 def create_bucket():
     bucket_name = request.form["bucket_name"]
-    s3 = boto3.resource('s3')
-    bucket = s3.create_bucket(Bucket=bucket_name)
+    #s3 = boto3.resource('s3')
+    #bucket = s3.create_bucket(Bucket=bucket_name)
+    #above commented and the below 2 lines of codes should
+    #be checked again and confirmed whether which 2 lines work. 
+    client = boto3.client("s3")
+    client.create_bucket(Bucket=bucket_name)
     
     return redirect("/")
 
